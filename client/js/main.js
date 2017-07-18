715e21789fa2d62e8d67cc371d63d609b2ef6c56
xineohp-f <phnxfaiz@gmail.com>
2017-07-19 01:25:15 +0530

    Initial Commit

---
+++ client/js/main.js
@@ -0,0 +1,110 @@
+var nick;
+var channel = window.location.href.substring(window.location.href.lastIndexOf('/')+1);
+var socket = io();
+var v;
+
+function grow(el) {
+  el.style.height = el.scrollHeight + "px";
+  el.style.marginTop = -(el.scrollHeight - 50) + "px";
+}
+
+function check(event) {
+  if (event.keyCode === 13) {
+    var n = document.getElementById('myNick').value;
+    if (validate(n)) {
+      socket.emit('join', {nick: n, channel: channel});
+    }
+    else notif("Your username should contain only letters and digits.");
+  }
+}
+
+socket.on('ready', function(data) {
+
+  v = new Vue({
+    el: '#app',
+    data: {
+      users: data.users,
+      messages: [],
+      nick: data.nick,
+      channel: data.channel,
+      msg: ''
+    },
+    created: function() {
+      socket.on('newUser', function(data) {
+        this.messages.push({
+          msg: data.msg + ' joined the chat!',
+          class: 'info'
+        });
+        this.users = data.users;
+      }.bind(this));
+      socket.on('left', function(data) {
+        this.messages.push({
+          msg: data.msg + ' left the chat!',
+          class: 'info'
+        });
+        this.users = data.users;
+      }.bind(this));
+      socket.on('newMsg', function(data) {
+        if (this.messages.length >= 1 && this.messages[this.messages.length-1].nick == data.nick) {
+          this.messages[this.messages.length-1].msg += '\n' + data.msg;
+        }
+        else this.messages.push({nick: data.nick, msg:data.msg, class:'message'});
+        setTimeout(function() {window.scrollTo(0,document.body.scrollHeight)}, 300);
+      }.bind(this));
+    },
+    methods: {
+      grow: function() {
+        let el = document.getElementById('tarea');
+        el.style.height = el.scrollHeight + "px";
+        el.style.marginTop = -(el.scrollHeight - 50) + "px";
+      },
+      send: function() {
+        socket.emit('newMsg', {msg:this.msg});
+        this.msg = "";
+        document.getElementById('tarea').style.height = "50px";
+        document.getElementById('tarea').style.marginTop = "0px";
+      }
+    }
+  });
+  document.getElementById('login').className += " hide-up";
+  setTimeout(function(){document.getElementById('login').style.display = "none";}, 300);
+  document.getElementById('sidebar').className += " slide-left";
+  document.getElementById('sidebar').style.display = "block";
+  document.getElementById('u-info').className += " slide-left";
+  document.getElementById('u-info').style.display = "block";
+  document.getElementById('chat-box').className += " slide-Up";
+  document.getElementById('chat-box').style.display = "block";
+  document.getElementById('tarea').focus();
+});
+
+socket.on('errorNick', function(data) {
+  notif(data.msg);
+  document.getElementById('myNick').value = "";
+  document.getElementById('myNick').focus();
+});
+
+function validate(t) {
+  return /^[a-zA-Z0-9]{1,24}$/.test(t);
+}
+
+function notif(msg) {
+  document.getElementById('notif').style.display = "block";
+  document.getElementById('notif').innerHTML = msg;
+  document.getElementById('notif').className = "notif";
+  document.getElementById('notif').className = "notif slide-down";
+  setTimeout("clearNotif()", 3000);
+}
+
+function clearNotif() {
+  document.getElementById('notif').innerHTML = "";
+  document.getElementById('notif').style.display = "none";
+}
+
+function checkC(event) {
+  if (event.keyCode === 13) {
+    if (validate(document.getElementById('myChannel').value)) {
+      window.location.href="/"+document.getElementById('myChannel').value;
+    }
+    else notif("Channel name can contain only letters and digits.");
+  }
+}
---
+++ server.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port);
+console.log("Server started on port " + port);
+
+app.use(express.static(__dirname + '/client'));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+
+app.get('/:id', function(req, res) {
+	if (validate(req.params.id)) {
+    res.sendFile(__dirname + '/client/chat.html');
+  }
+	else res.sendFile(__dirname + '/client/index.html');
+})
+
+var CLIENTS = [];
+
+io.on('connection', function(socket) {
+  console.log('New Connection: ' + socket.id);
+  CLIENTS[socket.id] = new client(socket);
+
+  socket.on('join', function(data) {
+    if (validate(data.nick) && validate(data.channel) && !dupNick(data.nick, data.channel)) {
+      CLIENTS[socket.id].nick = data.nick;
+      CLIENTS[socket.id].channel = data.channel;
+      let us = getUsers(data.channel);
+      socket.emit('ready', {nick: data.nick, channel: data.channel, users: us});
+			broadcast('newUser',CLIENTS[socket.id]);
+    }
+    else socket.emit('errorNick', {msg: "Check your nickname or channel name. It should only contain letters and digits."});
+  });
+
+	socket.on('newMsg', function(data) {
+		if(data.msg.length > 60000) return;
+		data.msg = data.msg.replace(/\s+/, ' ');
+		if(data.msg != '' && data.msg != ' ')
+			send('newMsg', CLIENTS[socket.id], data.msg);
+	});
+
+  socket.on('disconnect', function() {
+		var t = {channel: CLIENTS[socket.id].channel, nick: CLIENTS[socket.id].nick};
+		CLIENTS[socket.id] = null;
+		broadcast('left',t);
+    console.log('Disconnected: ' + socket.id);
+  });
+});
+
+function send(type, clt, data) {
+	for (key in CLIENTS) {
+    if (CLIENTS[key] && CLIENTS[key] != null) {
+      if (CLIENTS[key].channel == clt.channel)
+				CLIENTS[key].socket.emit(type, {nick:clt.nick, msg:data});
+    }
+  }
+}
+
+function broadcast(type, clt) {
+	for (key in CLIENTS) {
+    if (CLIENTS[key] && CLIENTS[key] != null) {
+      if (CLIENTS[key].channel == clt.channel && CLIENTS[key].nick != clt.nick)
+				CLIENTS[key].socket.emit(type, {msg: clt.nick, users:getUsers(clt.channel)});
+    }
+  }
+}
+
+function getUsers(chan) {
+  let us = [];
+  for (key in CLIENTS) {
+    if (CLIENTS[key] && CLIENTS[key] != null) {
+      if (CLIENTS[key].channel == chan)
+        us.push(CLIENTS[key].nick);
+    }
+  }
+  return us;
+}
+
+function dupNick(nick, chan) {
+  for (key in CLIENTS) {
+    if (CLIENTS[key] && CLIENTS[key] != null)
+      if (CLIENTS[key].channel == chan && CLIENTS[key].nick == nick) return true;
+  }
+  return false;
+}
+
+function client(socket) {
+	this.socket = socket;
+  this.nick = "";
+  this.channel = "";
+}
+
+function validate(txt) {
+  return /^[a-zA-Z0-9]{1,24}$/.test(txt);
+}
